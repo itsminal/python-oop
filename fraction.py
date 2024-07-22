a247d0b4d6fadae5fa69a1fa3ee57b6b850489b1
Minal Saindane <120405098+itsminal@users.noreply.github.com>
2024-07-22 17:51:46 +0530

    Update fraction.py

--- fraction.py
+++ fraction.py
@@ -1,4 +1,9 @@
-class Fraction:
-    def __init__(self, n, d):
-        self.num = n
-        self.den = d
+from main import Atm
+
+c1 = Atm()
+c2 = Atm()
+c3 = Atm()
+
+print(c1.s_no)
+print(c2.s_no)
+print(c3.s_no)
